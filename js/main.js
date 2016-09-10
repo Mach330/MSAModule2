0a925ea3d8de3c385a536651b2a360928151df2c
max van duin <max van duin>
2016-09-10 12:16:55 +1200

    Added FB posting and large list of currencies

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/// <reference path="X:\MSA module 2\typings\index.d.ts" />
+var testbtn = $("#testbtn")[0];
+testbtn.addEventListener("click", function () {
+    // TODO: Load random song based on mood
+    alert("You clicked the button again");
+});
+
+// Only works after `FB.init` is called
+function myFacebookLogin() {
+    FB.login(function () {
+        // Note: The call will only work if you accept the permission request
+        FB.api('/me/feed', 'post', { message: 'Hello, old world!' });
+    }, { scope: 'publish_actions' });
+}
