fb809162c91483ab8938f6f830fa02d746d32b7f
max van duin <max van duin>
2016-09-13 10:59:28 +1200

    the API logic is working, need to make it look pretty

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 /// <reference path="X:\MSA module 2\typings\index.d.ts" />
-var testbtn = $("#testbtn")[0];
-testbtn.addEventListener("click", function () {
-    // TODO: Load random song based on mood
-    alert("You clicked the button again");
-});
-
 // Only works after `FB.init` is called
-function myFacebookLogin() {
+function facebookLogin() {
     FB.login(function () {
         // Note: The call will only work if you accept the permission request
-        FB.api('/me/feed', 'post', { message: 'Hello, old world!' });
+        FB.api('/me/feed', 'post', { message: 'Hello, world!' });
     }, { scope: 'publish_actions' });
 }
+function MarketRate() {
+    var from = document.getElementById("from").value;
+    var URLandBase = "http://api.fixer.io/latest?base=" + from;
+    var url = URLandBase.replace("_CURR", "@Model.CurrencyId");
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        var resInBase = JSON.parse(xhr.responseText);
+        var amount = +document.getElementById("amount").value;
+        var to = document.getElementById("to").value;
+        if (to === from) {
+            exchange = +'1';
+        }
+        else {
+            exchange = +resInBase.rates[to];
+        }
+        //Drill down
+        document.getElementById('firstP').innerHTML = ("" + Math.round((exchange * amount * 100)) / 100);
+    };
+    xhr.onerror = function () {
+        //Process error
+    };
+    xhr.send();
+}
+;
+
+
+$(function(){
+    $(".chosen").chosen();
+});
--- js/main.ts
+++ js/main.ts
@@ -1,26 +1,43 @@
 /// <reference path="X:\MSA module 2\typings\index.d.ts" />
 declare var FB:any;
+declare var exchange:number;
 
 // Only works after `FB.init` is called
-function myFacebookLogin() {
+function facebookLogin() {
     FB.login(function(){
   // Note: The call will only work if you accept the permission request
   FB.api('/me/feed', 'post', {message: 'Hello, world!'});
 }, {scope: 'publish_actions'});
 }
+  
+function MarketRate() {
 
-function calculateRate() {
-	//get whats in the comboboxes
-	var from = document.getElementById("from").value;
+    var from = (<HTMLInputElement>document.getElementById("from")).value;
+    var URLandBase = `http://api.fixer.io/latest?base=${from}`;
 
-	var to = document.getElementById("to").value;
+    var url = URLandBase.replace("_CURR", "@Model.CurrencyId");
 
-	var amount = document.getElementById("amount").value;
-	//make dynamic API request
-	GET
-	//access the data that is recieved
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
 
-	//change the GUI to match what was recieved
+    xhr.onload = function () {
+        var resInBase = JSON.parse(xhr.responseText);
+        var amount = +(<HTMLInputElement>document.getElementById("amount")).value;
 
-	//
-}
+        var to = (<HTMLInputElement>document.getElementById("to")).value;
+        if( to === from){
+            exchange = +'1';
+        } else {
+            exchange = +resInBase.rates[to];
+        }
+
+        //Drill down
+        document.getElementById('firstP').innerHTML = ("" + Math.round((exchange * amount * 100))/100);
+    };
+
+    xhr.onerror = function () {
+        //Process error
+    };
+
+    xhr.send();
+};
